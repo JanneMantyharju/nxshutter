937f8076ca6fd40f0d965450a6c4e7d6c16208ab
Janne Mäntyharju <janne.mantyharju@iki.fi>
2013-10-30 14:35:35 +0200

    code changes

--- shutter/shutter.ino
+++ shutter/shutter.ino
@@ -1,13 +1,13 @@
 /* 
 	Shutter control for rc receiver
-	(c) Janne Mäntyharju 2013
+	(c) Janne Mï¿½ntyharju 2013
 	
 	Pins are for Atmel TINY2313 controller, modify as needed
 */
 
 #define RCIN 9
-#define FOCUS 11
-#define SHUTTER 10
+#define FOCUS 1
+#define SHUTTER 0
 
 #define FOCUS_ACTIVATE 1400
 #define SHUTTER_ACTIVATE 1700
@@ -16,16 +16,18 @@
 void setup()
 {
   pinMode(RCIN, INPUT);
-  pinMode(FOCUS, INPUT);
-  pinMode(SHUTTER, INPUT);
+  pinMode(FOCUS, OUTPUT);
+  pinMode(SHUTTER, OUTPUT);
   digitalWrite(FOCUS, LOW);
   digitalWrite(SHUTTER, LOW);
+  
 }
 
 void loop()
 {
+  UCSRB=0;
   int pwm;
-  
+
   pwm = pulseIn(RCIN, HIGH, 25000);
   if (pwm > FOCUS_ACTIVATE && pwm < SHUTTER_ACTIVATE) {
     pinMode(SHUTTER, INPUT);
